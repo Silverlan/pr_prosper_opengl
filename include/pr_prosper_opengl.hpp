11db43a6878ec0d0fd285a2033ae72ee81a077f0
Silverlan <silverlan@pragma-engine.com>
2025-11-12 18:57:54 +0100

    wip

--- include/pr_prosper_opengl.hpp
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: (c) 2020 Silverlan <opensource@pragma-engine.com>
-// SPDX-License-Identifier: MIT
-
-#ifndef __PR_PROSPER_OPENGL_HPP__
-#define __PR_PROSPER_OPENGL_HPP__
-
-#endif
