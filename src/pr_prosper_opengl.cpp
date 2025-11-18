990d1cd0d9c128cdcb8c9b22d84fa821dfc0914a
Silverlan <silverlan@pragma-engine.com>
2025-11-18 20:42:00 +0100

    refactor: migrate library to C++20 modules

--- src/pr_prosper_opengl.cpp
+++ src/pr_prosper_opengl.cpp
@@ -1,17 +1,10 @@
 // SPDX-FileCopyrightText: (c) 2020 Silverlan <opensource@pragma-engine.com>
 // SPDX-License-Identifier: MIT
 
-#include <prosper_opengl_definitions.hpp>
-#include <gl_context.hpp>
-
-#ifdef __linux__
-#define DLLEXPORT __attribute__((visibility("default")))
-#else
-#define DLLEXPORT __declspec(dllexport)
-#endif
+import pragma.prosper.opengl;
 
 extern "C" {
-DLLEXPORT bool initialize_render_api(const std::string &engineName, bool enableValidation, std::shared_ptr<prosper::IPrContext> &outContext, std::string &errMsg)
+PR_EXPORT bool initialize_render_api(const std::string &engineName, bool enableValidation, std::shared_ptr<prosper::IPrContext> &outContext, std::string &errMsg)
 {
 	outContext = prosper::GLContext::Create(engineName, enableValidation);
 	return true;
